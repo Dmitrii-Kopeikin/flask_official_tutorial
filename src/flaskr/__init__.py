40975c4784298e342d7dd707958afb96a228305d
Dmitrii-Kopeikin <kopeikindp@gmail.com>
2023-04-07 20:11:27 +0600

    Basic authentication/authorization added

--- src/flaskr/__init__.py
+++ src/flaskr/__init__.py
@@ -23,6 +23,9 @@ def create_app(test_config=None):
     except OSError:
         pass
 
+    from . import auth
+    app.register_blueprint(auth.bp)
+
     @app.route('/hello')
     def hello():
         return "Hello, World!"
